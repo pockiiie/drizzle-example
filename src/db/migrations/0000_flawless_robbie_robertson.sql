b94181aecb7509f5ca0cf1a92a5d2a20905ab998
pockiiie <pockiemail@gmail.com>
2024-10-18 09:35:42 +0700

    moved migrations folder

--- src/db/migrations/0000_flawless_robbie_robertson.sql
+++ src/db/migrations/0000_flawless_robbie_robertson.sql
@@ -4,7 +4,15 @@ CREATE TABLE IF NOT EXISTS "sessions" (
 	"expires_at" timestamp NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "password" text NOT NULL;--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" varchar(255) NOT NULL,
+	"age" integer NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" text NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
